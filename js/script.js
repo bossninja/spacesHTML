aa50af4f7eb578062a1ca10ce520d218c711c9eb
Lucky Sebastian <luckyrps@yahoo.com>
2012-05-17 23:33:23 +0800

    initial index page markup

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@
 
 */
 
+$(document).ready(function() {
+	$('.panel-button .button').click(function(e) {
+		e.preventDefault();
 
+		var arrow = '<span class="panel-button-arrow"></span>';
+		var txt = $(this).html() == arrow + 'View Properties' ? arrow + 'Close Properties' : arrow + 'View Properties';
+		$(this).html(txt);
 
-
-
-
-
+		$('.logos').animate({height: 'toggle'});
+	});
+});
